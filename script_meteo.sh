4fd52ff88f113530aaebcea296465cd617a34869
GoldenSpace6 <74157253+GoldenSpace6@users.noreply.github.com>
2022-12-27 22:35:02 +0100

    Add files via upload

---
+++ script_meteo.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+p=0
+t=0
+h=0
+m=0
+w=0
+region=0
+sort="avl"
+#dmin=0
+#dmax=0
+
+#g=0
+#a=0
+
+while getopts "f:t:p:whmFGSAOQ-:" option; do
+    case "${option}" in
+        f)
+            if [ ! -f "${OPTARG}" ] | [ ! *.csv == "${OPTARG}" ] #csv
+            then
+                echo wrong file
+                exit 1
+            fi
+            file=${OPTARG}
+            ;;
+        t)
+            echo ${OPTARG}
+            t=${OPTARG}
+            #((t == 15 || t == 75)) || echo lol
+            ;;
+        p)
+            p=${OPTARG}
+            ;;
+        w)
+            w=1
+            ;;
+        h)
+            h=1
+            ;;
+        m)
+            m=1
+            ;;
+        F | G | S | A | O | Q)
+            region=${option}
+            ;;
+        d)
+            ;;
+        -)
+            case "${OPTARG}" in
+                help)
+                    echo try random thing, it might work
+                    exit 0
+                    ;;
+                tab | avl | abr)
+                    sort=${OPTARG}
+                    ;;
+                *)
+                    echo Wrong Argument on -${OPTARG}
+                    ;;
+            esac
+            ;;
+        *)
+            echo Wrong Argument on -${OPTARG}
+            ;;
+    esac
+done
+#shift $((OPTIND-1))
+echo "p = ${p}
+t = ${t}
+h = ${h}
+w = ${w}
+m = ${m}
+region = ${region}
+sort = ${sort}"
