40ee6e766d37f4a08c165160ae2bc351969d5471
GoldenSpace6 <74157253+GoldenSpace6@users.noreply.github.com>
2023-01-12 10:09:30 +0100

    Update script_meteo.sh

--- script_meteo.sh
+++ script_meteo.sh
@@ -155,6 +155,14 @@ fi
 
 # ---- Filtering Data ---- V2
 ffile=$file
+
+#awk -F";" '{print substr( $0, 0, index($0,";")+1 )    substr($0, index(substr($0,0,index($0,";")+2),";")+1, 10000 ) }' meteo_filtered_data_v1.csv
+
+
+awk -F";" '{$2=substr(system("date -d "substr($2,0,10)" +%s"),5,30);print $0}' meteo_filtered_data_v1.csv
+
+awk -F";" '{print $0";"substr(system("date -d "substr($2,0,10)" +%s"),5,30)}' meteo_filtered_data_v1.csv
+
 if [ -n "$d" ] || [ -n "$region" ];then
     #Date
     if [ -n "$d" ]; then
@@ -163,6 +171,29 @@ if [ -n "$d" ] || [ -n "$region" ];then
         dmin=$(date -d $1 +%s)
         dmax=$(date -d $2 +%s)
     fi
+    for i in $loop;do
+        IFS=";"
+        HOLA=(${i})
+
+        #echo "$1 ; $2 ; $3 ; $4 ; $5 ; $6 ; $7 ; $8 ; $9 ; ${10}"
+        save=1
+        lm=$(( lm + 1 ))
+
+        if [ $lm -eq "20" ];then
+            echo $ght
+            lm=0
+            ght=$(( ght + 1 ))
+        fi
+        
+        #Date
+        if [ -n "$d" ]; then
+            tempdate=`date -d ${HOLA[1]} +%s`
+            if (( $tempdate < $dmin || $tempdate > $dmax )); then
+                save=0
+            fi
+        fi
+    done
+
     #Region
     if [ -n "$region" ]; then
         case $region in
@@ -184,6 +215,14 @@ if [ -n "$d" ] || [ -n "$region" ];then
                 echo Error Wrong region
                 exit 1
         esac
+        dd=$(date -d $5)
+        #awk -F";" -v '{print substr($2,0,10)}' $ffile > filetemp.csv
+
+        #awk -F";" -v date="$(date +%Y-%m-%d)" -v temp="(substr($2,0,10))" '{print temp}' meteo_filtered_data_v1.csv 
+
+        #awk -F";" '{test=substr($2,0,10);print $test}' meteo_filtered_data_v1.csv 
+    
+
         awk -F";" 'substr($10,0,index($10,",")-1)-('${Area[0]}')>0 && substr($10,0,index($10,",")-1)-('${Area[1]}')<0 && substr($10,index($10,",")+1,100)-('${Area[2]}')>0 && substr($10,index($10,",")+1,100)-('${Area[3]}')<0 {print $0}' $ffile > filetemp.csv
         ffile=filetemp.csv
     fi
